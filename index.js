38799bf51d4ddc4885488fd1b266ad6b1ff7f31f
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-04-03 14:25:23 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o github");
 
 turboLogger("estou aprendendo o fluxo do git");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
